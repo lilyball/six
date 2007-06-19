225550b67aa3ad6985e4020fd0e58d31abfabae3
Caius Durling <dev_at_caius_dot_name>
2007-06-19 05:52:43 +0100

    Global Logging Class
    Global logging class, adds the timestamp to the start of the log line.

---
+++ lib/logging.rb
@@ -0,0 +1,28 @@
+# 
+#  logging.rb
+#  Core Logging Class for Cybot
+#  
+#  Created by Caius Durling <dev at caius dot name> on 2007-06-18.
+#  
+
+class Logging
+    
+  def initialize
+    @disable_time = false
+  end
+  
+  def puts(str)
+    Kernel::puts(now + str.to_s)
+    @disable_time = false
+  end
+  
+  def print(str)
+    Kernel::print(now + str.to_s)
+    @disable_time = !str.to_s.include?("\n")
+  end
+  
+private
+  def now
+    @disable_time ? '' : Time.now.strftime('%Y-%m-%d %H:%m:%S ')
+  end
+end
