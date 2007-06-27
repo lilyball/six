c4d2132f17326b3d49bc2032b48364d72a9cf999
Ciaran Walsh <ciaran@ciaranmbp.local>
2007-06-27 03:17:28 +0100

    Updated to use Async.run

--- plugins/web.rb
+++ plugins/web.rb
@@ -2,6 +2,7 @@
 # Web services plugin. Google and more.
 #
 
+require 'async'
 require 'net/http'
 require 'uri'
 require 'htmlentities'
@@ -18,16 +19,13 @@ class Web < PluginBase
   def cmd_title(irc, line)
     if !line or line.empty?
       irc.reply 'USAGE: title <http-url>'
-    elsif @requests >= @max_requests
-      irc.reply 'My limit of outstanding web queries has been reached. Try again in a moment.'
     else
       line = "http://#{line}" unless line =~ /^http:\/\//
       if !(uri = URI.parse(line)) or !uri.kind_of?(URI::HTTP)
         irc.reply 'Error parsing URL.  For now, only HTTP URLs are accepted.'
       else
-        Thread.new do
+        Async.run(irc) do
           begin
-            @requests += 1
             Net::HTTP.start(uri.host, uri.port) do |http|
               buffer = ''
               path = uri.path
@@ -42,8 +40,6 @@ class Web < PluginBase
             end
           rescue SocketError
             irc.reply 'Error connecting to host.'
-          ensure
-            @requests -= 1
           end
         end
       end
@@ -57,35 +53,26 @@ class Web < PluginBase
       irc.reply 'USAGE: google <search string>'
       return
     end
-    if @requests >= @max_requests
-      irc.reply 'My limit of outstanding web queries has been reached. Try again in a moment.'
-      return
-    end
 
     # Let's google!
-    Thread.new do
-      begin
-        @requests += 1
-        Net::HTTP.start('www.google.com') do |http|
-          search = line.gsub(/[^a-zA-Z0-9_\.\-]/) { |s| sprintf('%%%02x', s[0]) }
-          re = http.get("/search?ie=utf8&oe=utf8&q=#{search}", 
-            { 'User-Agent' => 'CyBrowser' })
-          if re.code == '200'
-            if re.body =~ /<a href="([^"]+)" class=l>(.+?)<\/a>/
-              link = $1.decode_entities
-              desc = $2.gsub('<b>', "\x02").gsub('</b>', "\x0f").decode_entities
-              irc.reply "#{link} (#{desc})"
-            elsif re.body =~ /did not match any documents/
-              irc.reply 'Nothing found.'
-            else
-              irc.reply "Error parsing Google output."
-            end
+    Async.run(irc) do
+      Net::HTTP.start('www.google.com') do |http|
+        search = line.gsub(/[^a-zA-Z0-9_\.\-]/) { |s| sprintf('%%%02x', s[0]) }
+        re = http.get("/search?ie=utf8&oe=utf8&q=#{search}", 
+          { 'User-Agent' => 'CyBrowser' })
+        if re.code == '200'
+          if re.body =~ /<a href="([^"]+)" class=l>(.+?)<\/a>/
+            link = $1.decode_entities
+            desc = $2.gsub('<b>', "\x02").gsub('</b>', "\x0f").decode_entities
+            irc.reply "#{link} (#{desc})"
+          elsif re.body =~ /did not match any documents/
+            irc.reply 'Nothing found.'
           else
-            irc.reply "Google returned an error: #{re.code} #{re.message}"
+            irc.reply "Error parsing Google output."
           end
+        else
+          irc.reply "Google returned an error: #{re.code} #{re.message}"
         end
-      ensure
-        @requests -= 1
       end
     end
 
@@ -93,6 +80,5 @@ class Web < PluginBase
   help :google, 'Searches the web with Google, returning the first result.'
   alias_method :cmd_lucky, :cmd_google
   help :lucky, "Alias for 'google'. Type 'google?' for more information."
-
 end
 
